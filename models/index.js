6ee04d31c8771dc870d1350cb7feec553e81f875
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-02-27 16:17:17 -0500

    Comment model and routes added

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment')
 
 // associations
 
@@ -21,6 +22,11 @@ User.hasMany(Vote, {
     foreignKey: 'user_id'
 })
 
+// Users can have many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
 // Posts only belong to one user
 Post.belongsTo(User, {
     foreignKey: 'user_id'
@@ -38,14 +44,29 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 })
 
+// Each post can have multiple comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
 // Each vote belongs to only one user
 Vote.belongsTo(User, {
     foreignKey: 'user_id'
 })
 
-// Each vote can only be to one post
+// Each vote can only belong to one post
 Vote.belongsTo(Post, {
     foreignKey: 'post_id'
 })
 
-module.exports = { User, Post, Vote };
+// Each comment can only belong to one user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+// Each comment can only belong to one post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+module.exports = { User, Post, Vote, Comment };
