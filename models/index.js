c6b7b2d8ed18372d1a0353cdd97fd0ea2ce64b70
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-02-27 14:57:41 -0500

    added Vote model and routes

--- models/index.js
+++ models/index.js
@@ -1,13 +1,51 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // associations
+
+// Users can have multiple posts
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 
+// Users can be associated with many posts by voting on them
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+// Users can vote on multiple posts
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+})
+
+// Posts only belong to one user
 Post.belongsTo(User, {
     foreignKey: 'user_id'
 })
 
-module.exports = { User, Post };
+// Posts can be associated with multiple users through user votes
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+})
+
+// Posts can have multiple votes
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+})
+
+// Each vote belongs to only one user
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+// Each vote can only be to one post
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+module.exports = { User, Post, Vote };
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
 
 // GET /api/posts
 router.get('/', (req, res) => {
   Post.findAll({
-    attributes: ['id', 'title', 'post_text', 'user_id', 'created_at'],
+    attributes: [
+      'id',
+      'title',
+      'post_text',
+      'user_id',
+      'created_at',
+      [`(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)`,
+      'vote_count',]
+    ],
     order: [['created_at', 'DESC']],
     include: [
       {
@@ -26,7 +35,15 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ['id', 'title', 'post_text', 'user_id', 'created_at'],
+    attributes: [
+      'id',
+      'title',
+      'post_text',
+      'user_id',
+      'created_at',
+      [`(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)`,
+      'vote_count',]
+    ],
     include: [
       {
         model: User,
@@ -59,6 +76,17 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+  // static method in models/Post.js
+  Post.upvote(req.body, {Vote})
+      .then(updatedPostData => res.json(updatedPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+  });
 });
 
 // PUT /api/posts/i
